11dbbb27900cf646907c2e562ab5096bf4e2534c
Ctorres2020 <cesarivantorres20@gmail.com>
2025-01-20 09:29:33 -0300

    se agrego cambios

--- server.js
+++ server.js
@@ -13,10 +13,12 @@ app.use('/api/posts', postsRoutes);
 app.use('/api/users', usersRoutes);
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs))
 
+// mongoose.connect("mongodb://localhost:27017", { dbName: 'demo_db'})
+
 mongoose.connect(process.env.MONGO_URL, { dbName: 'demo_db'})
     .then(() => {
         console.log('Connected to MongoDB')
-        app.listen(process.env.PORT, 'localhost', () => console.log('Server is running on http://localhost:4000'));
+        app.listen(process.env.PORT, 'localhost', () => console.log(`Server is running on http://localhost:${process.env.PORT}`));
     })
     .catch(err => console.log(err));
 
